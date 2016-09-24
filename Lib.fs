a72bc7d7f1540f59ba5fb32c38bc7f62db068e96
Maria Lopez <mane@Johns-MacBook-Air-2.local>
2016-09-24 11:03:06 -0400

    baseline

---
+++ Lib.fs
@@ -0,0 +1,5 @@
+namespace ChynoSurf
+
+module Lib =
+    let f x = x + 1
+    let g x = x * x
---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+namespace ChynoSurf
+open System
+
+module Program =
+    [<EntryPoint>]
+    let main argv = 
+        printfn "Hello World!"
+        argv 
+        |> Array.map int
+        |> Array.map Lib.g
+        |> Array.iter (printf "Valueis : %i \n")
+        0 // return an integer exit code
